e35195e8f8675fa5924bd9364c9e98b0eb543f37
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2021-12-11 13:44:12 +0800

    fix

--- Template/Plugin.cpp
+++ Template/Plugin.cpp
@@ -4,26 +4,10 @@
 #include <MC/Player.hpp>
 #include <MC/ItemStack.hpp>
 
-bool PlayerAttack(Event::PlayerAttackEvent event)
-{
-	ItemStack *bread = ItemStack::create("minecraft:bread", 1);
-	event.mPlayer->giveItem(bread);
-	return true;
-}
 
-void PluginMain()
-{
-	Logger::setTitle("MyPlugin");
-	Logger::Info("Hello world");
-	Logger::Warn("My first BDS plugin!");
-
-	Event::PlayerAttackEvent::subscribe(PlayerAttack);
-}
 
 void PluginInit()
 {
-	Event::ServerStartedEvent::subscribe([](Event::ServerStartedEvent ev) -> bool {
-		PluginMain();
-		return true;
-	});
+
+
 }
--- Template/dllmain.cpp
+++ Template/dllmain.cpp
@@ -1,9 +1,9 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
-#pragma comment(lib, "Lib/bedrock_server_api.lib")
-#pragma comment(lib, "Lib/bedrock_server_var.lib")
-#pragma comment(lib, "Lib/SymDBHelper.lib")
-#pragma comment(lib, "Lib/LiteLoader.lib")
+#pragma comment(lib, "../SDK/Lib/bedrock_server_api.lib")
+#pragma comment(lib, "../SDK/Lib/bedrock_server_var.lib")
+#pragma comment(lib, "../SDK/Lib/SymDBHelper.lib")
+#pragma comment(lib, "../SDK/Lib/LiteLoader.lib")
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
