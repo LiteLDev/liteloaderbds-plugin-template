6539a20560615ed2b9b92d6eb299d95520ab8732
yqs112358 <1425321705@qq.com>
2021-12-20 20:54:03 +0800

    modify template

--- Template/Plugin.cpp
+++ Template/Plugin.cpp
@@ -3,11 +3,14 @@
 #include <LoggerAPI.h>
 #include <MC/Player.hpp>
 #include <MC/ItemStack.hpp>
+#include <LLAPI.h>
 
-static_assert(false, "Please set your global logger title and delete this line");
-Logger logger("Template");
+static_assert(false, "Please set your plugin's name below and delete this line");
+Logger logger("PluginName");
 
 void PluginInit()
 {
+	static_assert(false, "Please modify your plugin registration details and delete this line");
+	LL::registerPlugin("PluginName", "Introduction", LL::Version(1, 0, 0));
 
 }
--- Template/dllmain.cpp
+++ Template/dllmain.cpp
@@ -1,6 +1,5 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
-#include <LLAPI.h>
 #pragma comment(lib, "../SDK/Lib/bedrock_server_api.lib")
 #pragma comment(lib, "../SDK/Lib/bedrock_server_var.lib")
 #pragma comment(lib, "../SDK/Lib/SymDBHelper.lib")
@@ -14,9 +13,6 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        static_assert(false, "Please modify your plugin registration details and delete this line");
-        LL::registerPlugin("name", "introduction", LL::Version(1, 0, 0));
-        break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
