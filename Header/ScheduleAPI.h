f399ae82e2ec793da4bac0b99d161eb824881f2f
yqs112358 <1425321705@qq.com>
2021-12-20 20:50:05 +0800

    Squashed 'SDK/' content from commit bf31b59
    git-subtree-dir: SDK
    git-subtree-split: bf31b5906a9cef222624edfb48844a32225b2693

---
+++ Header/ScheduleAPI.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "Global.h"
+#include <functional>
+
+///////////////////////////////////////////////////////
+// Schedule future callback plans
+//
+// [Usage]
+//
+//   Schedule::delay([](){
+//      Logger::Error("hello");
+//   }, 20);                        // Delay this callback for 20 ticks (20 ticks = 1 second)
+//
+//   Schedule::repeat([](){
+//      Logger::Error("hello");
+//   }, 40);                        // Schedule this callback once per 40 ticks (20 ticks = 1 second)
+//
+//   auto sche = Schedule::delayRepeat([](){
+//      Logger::Error("hello");
+//   }, 20, 60);                    // Delay first call to callback for 20 ticks
+//                                  // then schedule this callback once per 60 ticks (20 ticks = 1 second)
+//
+//   //......
+//   sche.cancel();                 // Cancel the schedule
+//
+/////////////////////////////////////////////////////
+
+class ScheduleTask
+{
+    friend class ScheduleTaskQueueType;
+
+    unsigned int taskId;
+    int leftTime, interval, count;
+    std::function<void(void)> task;
+
+public:
+    LIAPI bool cancel();
+    LIAPI ScheduleTask(unsigned long long delay, unsigned long long interval, int count);
+
+    inline unsigned int getTaskId()
+    {
+        return taskId;
+    }
+
+    inline bool operator<(const ScheduleTask& t) const
+    {
+        return leftTime < t.leftTime;
+    }
+};
+
+namespace Schedule
+{
+    LIAPI ScheduleTask delay(std::function<void(void)> task, unsigned long long tickDelay);
+    LIAPI ScheduleTask repeat(std::function<void(void)> task, unsigned long long tickInterval, int maxCount = -1);
+    LIAPI ScheduleTask delayRepeat(std::function<void(void)> task, unsigned long long tickDelay, unsigned long long tickInterval, int maxCount = -1);
+    LIAPI ScheduleTask nextTick(std::function<void(void)> task);
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+# LiteLoaderSDK
+> Plugin Development SDK of [LiteLoaderBDS](https://github.com/LiteLDev/LiteLoaderBDS)  
+
+Help you to develop strong BDS C++ plugins easily  
+More Info 👉 [LiteLoaderBDS](https://github.com/LiteLDev/LiteLoaderBDS)  
+
+<br>
+
+### Warning
+
+**NEVER** push to this repository **DIRECTLY**!  
+Which may cause the SDK to be out of sync in project LiteLoaderBDS.
+
+If modification is needed, just use `UploadSDK.cmd` project LiteLoaderBDS
