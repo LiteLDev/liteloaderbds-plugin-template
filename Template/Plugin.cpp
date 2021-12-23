01084c53a5266a7d9400aae3886471c496a1d7d7
yqs112358 <1425321705@qq.com>
2021-12-23 10:16:21 +0800

    Update Plugin.cpp

--- Template/Plugin.cpp
+++ Template/Plugin.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
 #include <EventAPI.h>
 #include <LoggerAPI.h>
+#include <MC/Level.hpp>
+#include <MC/BlockInstance.hpp>
+#include <MC/Block.hpp>
+#include <MC/BlockSource.hpp>
+#include <MC/Actor.hpp>
 #include <MC/Player.hpp>
 #include <MC/ItemStack.hpp>
 #include <LLAPI.h>
