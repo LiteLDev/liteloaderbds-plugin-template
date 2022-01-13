10d5b38fe92a05030d38f8d8ad56644eb35c4fa8
RimuruChan <179793046@qq.com>
2022-01-13 18:50:40 +0800

    add lib prebuild for cmake

--- Template/Plugin.cpp
+++ Template/Plugin.cpp
@@ -17,5 +17,4 @@ void PluginInit()
 {
 	static_assert(false, "Please modify your plugin registration details and delete this line");
 	LL::registerPlugin("PluginName", "Introduction", LL::Version(1, 0, 0));
-
 }
