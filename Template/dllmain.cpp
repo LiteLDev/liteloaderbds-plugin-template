33ff7a1f67edf836b55f7a928923fa230ce50f44
Jasonzyt <2126931871@qq.com>
2022-02-07 14:46:31 +0800

    Add #include<LLAPI.h>

--- Template/dllmain.cpp
+++ Template/dllmain.cpp
@@ -1,5 +1,6 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
+#include <LLAPI.h>
 #pragma comment(lib, "../SDK/Lib/bedrock_server_api.lib")
 #pragma comment(lib, "../SDK/Lib/bedrock_server_var.lib")
 #pragma comment(lib, "../SDK/Lib/SymDBHelper.lib")
