cdc8918184bd33305a84528c1a8f38b546964ad9
hackbiter <62537288+hackbiter@users.noreply.github.com>
2022-07-18 11:39:13 +0800

    fix bugs
    fix can not generate lib in windows

--- prepareLib.cmd
+++ prepareLib.cmd
@@ -1,8 +1,8 @@
-if not exist %1\SDK\Lib\bedrock_server_api.lib goto process
-if not exist %1\SDK\Lib\bedrock_server_var.lib goto process
+if not exist .\SDK\Lib\bedrock_server_api.lib goto process
+if not exist .\SDK\Lib\bedrock_server_var.lib goto process
 goto end
 
 :process
-cd /d %1\SDK\Tools\
+cd .\SDK\Tools\
 LibraryBuilder.exe -o ..\Lib\
 :end
